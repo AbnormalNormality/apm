1eef56fefeb4b8b2844d4ebf6697d94e91a4d86f
Alia Driscoll <dan.driscoll@aussiebb.com.au>
2025-04-21 12:02:32 +1000

    0.1.11

--- apm/__init__.py
+++ apm/__init__.py
@@ -1,4 +1,4 @@
-from typing import Callable, TypeVar, ParamSpec
+from typing import Any, Callable, TypeVar, ParamSpec
 
 
 P = ParamSpec("P")
@@ -8,14 +8,57 @@ R = TypeVar("R")
 class FunctionRegistry:
     def __init__(self):
         self.events: dict[str, Callable[..., object]] = {}
+        self._pending_name: str | None = None
 
-    def __call__(self, func: Callable[P, R]) -> Callable[P, R]:
-        self.events[func.__name__] = func
-        return func
+    def __call__(self, arg: str | Callable[..., object]) -> Callable[..., object] | Callable[[Callable[..., object]], Callable[..., object]]:
+        if isinstance(arg, str):
+            self._pending_name = arg
+            return self
+        else:
+            func = arg
+            name = self._pending_name or func.__name__
+            self.events[name] = func
+            self._pending_name = None
+            return func
 
-    def __getattr__(self, name: str) -> Callable[..., object] | None:
-        if name in self.events:
+    def __getattr__(self, name: str) -> Callable[..., object]:
+        if name in self.__dict__:
+            return self.__dict__[name]
+
+        elif name in self.events:
             return self.events[name]
+
+        else:
+            return self._DefaultFunction()
+
+    class _DefaultFunction:
+        def __call__(self, *args: Any, **kwds: Any):
+            return None
+
+        def __bool__(self) -> bool:
+            return False
 
 
 __all__ = ["FunctionRegistry"]
+
+
+if __name__ == '__main__':
+    class Mailbox:
+        def __init__(self):
+            self.event = FunctionRegistry()
+
+    mailbox = Mailbox()
+
+    @mailbox.event
+    def on_mail(message: str):
+        print(message)
+
+    on_mail_event = mailbox.event.on_mail
+    if on_mail_event:
+        on_mail_event("Hello, World!")
+
+    @mailbox.event("on_spam")
+    def spam_handler():
+        return False
+
+    mailbox.event.on_spam()
--- docs/apm.md
+++ docs/apm.md
@@ -1,17 +1,25 @@
 # FunctionRegistry
 ```
+from apm import FunctionRegistry
+
+
 class Mailbox:
     def __init__(self):
         self.event = FunctionRegistry()
 
-
 mailbox = Mailbox()
 
-
 @mailbox.event
-def on_message(message: str):
+def on_mail(message: str):
     print(message)
 
+on_mail_event = mailbox.event.on_mail
+if on_mail_event:
+    on_mail_event("Hello, World!")
 
-mailbox.event.on_message("Hello, World!")
+@mailbox.event("on_spam")
+def spam_handler():
+    return False
+
+mailbox.event.on_spam()
 ```
