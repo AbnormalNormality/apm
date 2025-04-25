9ba9d249c6d4ee6046bc5f8e9dce05d2d7d7101d
Alia Driscoll <dan.driscoll@aussiebb.com.au>
2025-04-25 14:35:20 +1000

    0.1.16

--- apm/__init__.py
+++ apm/__init__.py
@@ -10,27 +10,26 @@ FUNCTION = Callable[..., object]
 
 
 class FunctionRegistry:
-    def __init__(self):
-        self.events: dict[str, FUNCTION] = {}
-        self._pending_name: str | None = None
+    def __init__(self, *valid_keys: str):
+        self._events: dict[str, FUNCTION] = {}
+
+        self._validKeys = valid_keys
 
     def __call__(self, arg: str | FUNCTION) -> FUNCTION | Callable[[FUNCTION], FUNCTION]:
         if isinstance(arg, str):
-            self._pending_name = arg
-            return self
+            def decorator(func: FUNCTION) -> FUNCTION:
+                return self._register(arg, func)
+            return decorator
+
         else:
-            func = arg
-            name = self._pending_name or func.__name__
-            self.events[name] = func
-            self._pending_name = None
-            return func
+            return self._register(arg.__name__, arg)
 
     def __getattr__(self, name: str) -> FUNCTION:
-        if name in self.__dict__:
+        if name in self.__dict__.keys() or name in self.__dir__():
             return self.__dict__[name]
 
-        elif name in self.events:
-            return self.events[name]
+        elif name in self._events:
+            return self._events[name]
 
         else:
             return self._DefaultFunction()
@@ -42,11 +41,22 @@ class FunctionRegistry:
         def __bool__(self) -> bool:
             return False
 
+    def _register(self, name: str, func: FUNCTION) -> FUNCTION:
+        if name in self.__dict__.keys() or name in self.__dir__():
+            raise ValueError(
+                f"The name {name} is used by the class {type(self).__name__}")
+
+        elif self._validKeys and name not in self._validKeys:
+            raise ValueError(f"{name} is not a valid key")
+
+        self._events[name] = func
+        return func
+
 
 def clear_console():
     if platform.startswith("linux") or platform == "darwin":
         system("clear")
-        
+
     elif platform.startswith("win"):
         system("cls")
 
@@ -59,7 +69,7 @@ if __name__ == '__main__':
 
     class Mailbox:
         def __init__(self):
-            self.event = FunctionRegistry()
+            self.event = FunctionRegistry("on_mail", "on_spam")
 
     mailbox = Mailbox()
 
@@ -73,6 +83,6 @@ if __name__ == '__main__':
 
     @mailbox.event("on_spam")
     def spam_handler():
-        return False
+        print("Spam")
 
     mailbox.event.on_spam()
